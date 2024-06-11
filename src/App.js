b881707d6696271e47d1c09a142e1517e1cbd5cf
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2024-06-11 01:15:46 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <h1>Weather App</h1>;
+  return (
+    <div>
+      <h1>Weather App</h1>
+
+      <footer>
+        <p>
+          This project was coded by {""}
+          <a
+            href="https://github.com/Danielafsp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Daniela Pereira
+          </a>
+          , open-sourced on {""}
+          <a
+            href="https://github.com/Danielafsp/final-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          , and hosted on{" "}
+          <a
+            href="https://stellar-malabi-4e825e.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
