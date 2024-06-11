27c11eb7b5ed9977229dc93cdf6ddd7ebf54b74b
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2024-06-11 01:30:19 +0100

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Weather App</h1>
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
 
-      <footer>
-        <p>
-          This project was coded by {""}
-          <a
-            href="https://github.com/Danielafsp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Daniela Pereira
-          </a>
-          , open-sourced on {""}
-          <a
-            href="https://github.com/Danielafsp/final-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          , and hosted on{" "}
-          <a
-            href="https://stellar-malabi-4e825e.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify
-          </a>
-        </p>
-      </footer>
+        <footer>
+          <p>
+            This project was coded by {""}
+            <a
+              href="https://github.com/Danielafsp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Daniela Pereira
+            </a>
+            , open-sourced on {""}
+            <a
+              href="https://github.com/Danielafsp/final-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            , and hosted on{" "}
+            <a
+              href="https://stellar-malabi-4e825e.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
