6d492b2d4bc878696d47c05230c597e3e4669eae
Danielafsp <135218019+Danielafsp@users.noreply.github.com>
2024-06-17 21:02:19 +0100

    customized the app

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
+      color="#f8f9fc"
       size={props.size}
       animate={true}
     />
